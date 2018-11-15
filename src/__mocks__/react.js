9020afc09bfccf2fcffe1d2f91d6bd3cad5eb4a5
Florent Lepretre <florent.lepretre@gmail.com>
2018-11-15 10:43:26 +0100

    fixup! test(useRedux): test useRedux method

--- src/__mocks__/react.js
+++ src/__mocks__/react.js
@@ -4,7 +4,9 @@ export const useContext = jest.fn(x => x);
 const states = [];
 export const useState = jest.fn(defaultValue => {
   states.push(defaultValue);
-  return [defaultValue, jest.fn(value => states[states.length -1] = value)];
+  const index = states.length -1;
+
+  return [defaultValue, jest.fn(value => states[index] = value)];
 });
 
 const effects = [];
