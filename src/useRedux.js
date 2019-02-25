53c97fd0b6c0075845220d571202ecf35fd00c0f
Florent Lepretre <florent.lepretre@gmail.com>
2019-02-25 12:43:10 +0100

    fix(build): use babel to build and fix it (#6)
    * fix build
    * use babel to build
    * add travis file

--- src/useRedux.js
+++ src/useRedux.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import { ReactReduxContext } from 'react-redux';
 
 export const useRedux = () => {
-  const store = useContext(ReactReduxContext);
+  const { store } = useContext(ReactReduxContext);
   const [reduxState, setReduxState] = useState(store.getState());
 
   useEffect(() => {
