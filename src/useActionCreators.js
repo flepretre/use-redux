42717e586cb83fe862da8e1cbc2e2a9a35154a82
Florent Lepretre <florent.lepretre@gmail.com>
2019-02-27 09:27:41 +0100

    perf(useActionCreators): do not subscribe to the store for nothing

--- src/useActionCreators.js
+++ src/useActionCreators.js
@@ -1,4 +1,11 @@
-import { useRedux } from './useRedux';
+import { useContext } from 'react';
+import { ReactReduxContext } from 'react-redux';
 
-export const useActionCreators = (...actionCreators) =>
-  useRedux(undefined, actionCreators).slice(1);
+export const useActionCreators = (...actionCreators) => {
+  const { store } = useContext(ReactReduxContext);
+  const { dispatch } = store;
+
+  return actionCreators.map(actionCreator => params =>
+    dispatch(actionCreator(params))
+  );
+};
